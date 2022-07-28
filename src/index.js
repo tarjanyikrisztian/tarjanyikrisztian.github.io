9fef5f7c1fa06ced17ea78ab5c198f13e0a79c9d
Tarjányi Krisztián <krisztian0218@gmail.com>
2022-07-28 15:43:14 +0200

    w

--- src/index.js
+++ src/index.js
@@ -4,7 +4,30 @@ import App from './App'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  /*<React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>*/
+    <div className="container">
+        <div className="pages">
+            <div className='waves'>
+                <div className="name">
+                    <p><b className="kr">Krisztián</b> Tarjányi <span>|</span>
+                    </p>
+                    <p className="cd"><i><b>Creative</b> Developer</i></p>
+                    <p className="cd"><i>Student</i></p>
+                </div>
+            </div>
+        </div>
+        <div className="pages">
+            <div className='waves flip'>
+                <div className="bio">BIO</div>
+            </div>
+        </div>
+        <div className="pages">
+            <section className="gridLayout">
+                <div className="card">HAVE A NICE DAY</div>
+                <div className="card">CONTACT</div>
+            </section>
+        </div>
+    </div>
 )
