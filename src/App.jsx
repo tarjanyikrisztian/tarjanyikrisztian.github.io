95da72f765bb16a1936b70e74bb06f267f7a0f32
Tarjányi Krisztián <krisztian0218@gmail.com>
2022-08-07 03:16:03 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = ({ }) => {
                 <div className="name">
                     <p className="cursor"><TypedKr /></p>
                     <motion.p initial={{ opacity: 0, y: -25 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 2.3 }} className="cd">
-                        <b>Creative</b> <a className="kr">Developer</a>
+                        <b>Creative</b> <a className="kr">Developer,</a>
                     </motion.p>
                     <motion.p initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 5 }} transition={{ delay: 2.5 }} className="cd ">
                         <a className="kr">Student</a>
